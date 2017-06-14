5b0722aad6723f9daa8ca2666bd3f233f8c36e98
Daniel Katz <daniekatz@gmail.com>
2017-06-14 10:49:39 +0300

    Prepare a client for the api

---
+++ src/StyleKitSharper.Web/Controllers/ApiController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace StyleKitSharper.Web.Controllers
+{
+    [Route("api")]
+    public class ApiController : Controller
+    {
+        [HttpPost("transform")]
+        public IActionResult Transform([FromBody] string base64)
+        {
+            var bytes = Convert.FromBase64String(base64);
+            var java = Encoding.UTF8.GetString(bytes);
+
+            return Content(java);
+        }
+    }
+}
---
+++ src/StyleKitSharper.Web/wwwroot/js/main.js
@@ -0,0 +1,32 @@
+﻿(function () {
+
+    $(document).ready(function () {
+
+        $("#btnConvert").on('click', onConvert);
+
+    });
+
+    var onConvert = function () {
+        var java = $(".java-input").val();
+
+        if (java.length > 0) {
+            var base64 = base64EncodingUTF8(java);
+
+            $.ajax({
+                type: "POST",
+                data: JSON.stringify(base64),
+                url: "api/transform",
+                contentType: "application/json",
+                success: function (data) {
+                    $(".csharp-output").text(data);
+                }
+            });
+        }
+    };
+
+    var base64EncodingUTF8 = function (str) {
+        var encoded = new TextEncoderLite('utf-8').encode(str);
+        var b64Encoded = base64js.fromByteArray(encoded);
+        return b64Encoded;
+    }
+})();
